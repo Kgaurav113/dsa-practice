373dca7753567d7a4ff49dd0161e08316caa6e2e
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2022-03-31 20:49:11 +0530

    npm practice
    npm practice

---
+++ index.js
@@ -0,0 +1,42 @@
+import {v4 as uuidv4} from 'uuid';
+let member1={
+    name:"prateek",
+    id:uuidv4()
+}
+let member2={
+    name:"Nrupul",
+    id:uuidv4()
+}
+let member3={
+    name:"Yogesh",
+    id:uuidv4()
+}
+let member4={
+    name:"varun bhatt",
+    id:uuidv4()
+}
+let member5={
+    name:"Ankush",
+    id:uuidv4()
+}
+let member6={
+    name:"ChandraShekhar",
+    id:uuidv4()
+}
+let member7={
+    name:"Swanand Kadam",
+    id:uuidv4()
+}
+let member8={
+    name:"Ankur Kayasth",
+    id:uuidv4()
+}
+let member9={
+  name:"Shashank Shekhar",
+  id:uuidv4()
+}
+let member10={
+    name:"Albert",
+    id:uuidv4()
+}
+console.log(member1,member2,member3,member4,member5,member6,member7,member8,member9,member10)
